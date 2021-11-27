2a8f710980336e68aa36e0952ad8e979d505b7c0
Marius De Allie <m.deallie@surgicalsafety.com>
2021-11-26 21:20:40 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f9e6f39e-3c96-45f3-bec6-5a970fb33cf7';
+export const MODEL_CLOSE_SEC = 2.5;
